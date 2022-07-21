2b5cc4971d0cb0212503597ad010a53c1fe0f737
jose.pereda <jose.pereda@gluonhq.com>
2022-07-21 23:46:54 +0200

    Dispatch menu event on Android from native

--- modules/javafx.graphics/src/main/native-glass/monocle/android/nativeBridge.h
+++ modules/javafx.graphics/src/main/native-glass/monocle/android/nativeBridge.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2012, 2020, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2012, 2022, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -29,6 +29,7 @@
 void androidJfx_setNativeWindow(ANativeWindow* nativeWindow);
 void androidJfx_setDensity(float nativeDensity);
 void androidJfx_gotTouchEvent (int count, int* actions, int* ids, int* xs, int* ys, int primary);
+void androidJfx_gotMenuEvent(int x, int y, int xAbs, int yAbs, boolean isKeyboardTrigger);
 void androidJfx_requestGlassToRedraw();
 
 #define GLASS_LOG_INFO(...)  ((void)__android_log_print(ANDROID_LOG_INFO,"GLASS", __VA_ARGS__))
